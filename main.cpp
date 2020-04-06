963df5e086966a5abdc7cc2d44459689543df7c8
Blake Ba <blake.elink@gmail.com>
2020-04-05 22:11:59 -0400

    BuildMap function can handle multiple instances in the same file.

--- main.cpp
+++ main.cpp
@@ -7,6 +7,7 @@
 #include <boost/algorithm/string.hpp>
 #include <boost/tokenizer.hpp>
 #include <fstream>
+#include <iomanip>
 #include <iostream>
 #include <map>
 #include <string>
@@ -25,6 +26,7 @@ const string kStopwordFilename("stop_words.txt");
 vector<string> LoadStopWords();
 StringVecMap BuildMap(const vector<string> &, vector<string> &);
 void PrintMap(ostream &, StringVecMap &);
+bool is_number(const string &s);
 
 int main() {
   // Initialize a vector to hold the stop words
@@ -41,13 +43,18 @@ int main() {
 // Description: Print a map<string, vector<Posting>> to an ostream
 // Return Type: void
 void PrintMap(ostream &os, StringVecMap &map) {
-  for (auto it : map) {
-    os << it.first << "\t";
-    for (auto posting : it.second) {
-      os << "id: " << posting.doc_id << "\t";
-      for (auto location : posting.location) {
-        os << location << " ";
+  for (auto map_iterator : map) {
+    os << left << setw(20) << map_iterator.first;
+    for (auto posting_vector_iterator : map_iterator.second) {
+      os << setw(0) << "Doc" << posting_vector_iterator.doc_id;
+      os << " [";
+      int num_postings = posting_vector_iterator.location.size();
+      for (int location_id = 0; location_id < num_postings; location_id++) {
+        os << posting_vector_iterator.location[location_id] << " ";
+        if (location_id != num_postings - 1)
+          os << ", ";
       }
+      os << "] ";
     }
     os << endl;
   }
@@ -70,12 +77,16 @@ StringVecMap BuildMap(const vector<string> &files, vector<string> &v_stop) {
       boost::tokenizer<> tok(line);
       // TODO: fix the tokenizer to not include numbers
       // Loop over tokens
-      for (auto it = tok.begin(); it != tok.end(); it++) {
+      for (auto word_iterator = tok.begin(); word_iterator != tok.end();
+           word_iterator++) {
         // convert token to lowercase
-        string lowercase = *it;
+        string lowercase = *word_iterator;
         boost::algorithm::to_lower(lowercase);
         // if word is a stop word, continue with next token
         if (count(v_stop.begin(), v_stop.end(), lowercase))
+          continue;
+        // if word is a number, continue with next token
+        if (is_number(lowercase))
           continue;
         // check whether term is already stored in map
         StringVecMap::iterator term_it = tmp_map.find(lowercase);
@@ -86,20 +97,20 @@ StringVecMap BuildMap(const vector<string> &files, vector<string> &v_stop) {
         } else {
           // Term was found. Set a flag for if a
           // posting for this doc_id already exists.
-          bool post_for_this_doc_id_exists = false;
+          bool post_for_this_doc_id_already_exists = false;
           int index_of_matching_post = -1;
           for (int post_index = 0; post_index < (*term_it).second.size();
                post_index++) {
             if (term_it->second[post_index].doc_id == doc_id) {
-              post_for_this_doc_id_exists = true;
+              post_for_this_doc_id_already_exists = true;
               index_of_matching_post = post_index;
             }
           }
-          if (!post_for_this_doc_id_exists) {
+          if (!post_for_this_doc_id_already_exists) {
+            // add this new word to the map
             tmp_map[lowercase].emplace_back(Posting(doc_id, -1));
           } else {
-            assert(post_for_this_doc_id_exists == true);
-            // add a second location to the location vector
+            // add an additional location to the location vector
             (tmp_map[lowercase])[index_of_matching_post].location.push_back(-1);
           }
         }
@@ -129,4 +140,11 @@ vector<string> LoadStopWords() {
   }
   f_stop.close();
   return v_words;
+}
+
+bool is_number(const string &s) {
+  string::const_iterator it = s.begin();
+  while (it != s.end() && isdigit(*it))
+    ++it;
+  return !s.empty() && it == s.end();
 }
