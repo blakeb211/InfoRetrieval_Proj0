a7048aeea5231bac99ff3ac1894652c636247499
Blake Ba <blake.elink@gmail.com>
2020-04-13 15:18:36 -0400

    Removed Google Test project

--- Tests/test.cpp
+++
@@ -1,9 +0,0 @@
-#include "../MapBuilder.h"
-#include "pch.h"
-
-TEST(MapBuilder_tests, load_files) {
-  MapBuilder mb{"../stop_words.txt", vector<string>{"Test_Input_file.txt"}};
-
-  EXPECT_EQ(1, 1);
-  EXPECT_TRUE(true);
-}
